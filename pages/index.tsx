7cc6b33057244daebaf6a1569d642642f6c02a50
Anderson Chen <twan0817@gmail.com>
2023-08-12 21:40:42 +0800

    feat: add a test action

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,25 @@
 import Layout from "../components/layout"
+import { useState } from 'react'
+import { IDKitWidget } from "@worldcoin/idkit"
+import type { ISuccessResult } from "@worldcoin/idkit";
+
+const appId = "app_staging_bab24ffa02b5eceb09f302496b646dda";
+const action = "test-action";
 
 export default function IndexPage() {
+	const [proof, setProof] = useState<ISuccessResult | null>(null);
+  
+  // This function will be called when the proof is received
+  const handleVerify = (result: ISuccessResult) => {
+		console.debug("handleVerify ", result);
+    setProof(result);
+  };
+  
+  // This function will be called when the widget is closed
+  const onSuccess = (result: ISuccessResult) => {
+    console.debug("onSuccess ", result);
+  };
+
   return (
     <Layout>
       <h1>NextAuth.js Example</h1>
@@ -9,6 +28,16 @@ export default function IndexPage() {
         <a href="https://next-auth.js.org">NextAuth.js</a> with {" "}
         <a href ="https://worldcoin.org/world-id">World ID</a> for authentication.
       </p>
-    </Layout>
+      <IDKitWidget
+        app_id={appId} // obtained from the Developer Portal
+        action={action} // this is your action name from the Developer Portal
+        onSuccess={onSuccess} // callback when the modal is closed
+        handleVerify={handleVerify} // optional callback when the proof is received
+        // credential_types={['orb', 'phone']} // the credentials you want to accept
+        // enableTelemetry // optional, defaults to false
+      >
+        {({ open }) => <button onClick={open}>Verify with World ID</button>}
+      </IDKitWidget>
+  </Layout>
   )
 }
